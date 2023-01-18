ca3ed08f6f7c27f99663c75537f3f66d8ebc6518
Roy Ash <roy.ash@moovit.com>
2023-01-18 14:34:40 +0200

    improvements

--- src/main/java/io/github/ashr123/progress/bar/WebController.java
+++ src/main/java/io/github/ashr123/progress/bar/WebController.java
@@ -1,6 +1,7 @@
 package io.github.ashr123.progress.bar;
 
 import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 import org.springframework.stereotype.Controller;
@@ -16,17 +17,41 @@ import java.util.Optional;
 @Validated
 public class WebController {
 	@GetMapping(value = "/{progress}", produces = "image/svg+xml")
-	public String generateProgress(@PathVariable @PositiveOrZero @Max(100) double progress,
-								   @RequestParam(required = false) Optional<String> title,
-								   @RequestParam(defaultValue = "100") @Positive double scale,
-								   @RequestParam(required = false) Optional<Double> width,
-								   @RequestParam(defaultValue = "428bca") String color,
-								   @RequestParam(defaultValue = "%") String suffix,
-								   @RequestParam(name = "maximum-fraction-digits", defaultValue = "2") @PositiveOrZero int maximumFractionDigits,
-								   Model model) {
+	public String generateProgress(
+			@PathVariable
+			@PositiveOrZero
+			@Max(100)
+			double progress,
+
+			@RequestParam(required = false)
+			Optional<String> title,
+
+			@RequestParam(defaultValue = "100")
+			@Positive
+			double scale,
+
+			@RequestParam(required = false)
+			Optional<@Positive Double> width,
+
+			@RequestParam(name = "title-color",
+					defaultValue = "#428bca")
+			@Pattern(regexp = "^#?(?:(?:[\\da-f]{3}){1,2}|(?:[\\da-f]{4}){1,2})$",
+					flags = Pattern.Flag.CASE_INSENSITIVE)
+			String titleColor,
+
+			@RequestParam(defaultValue = "%")
+			String suffix,
+
+			@RequestParam(name = "maximum-fraction-digits",
+					defaultValue = "2")
+			@PositiveOrZero
+			int maximumFractionDigits,
+
+			Model model
+	) {
 		model.addAttribute("title", title)
 				.addAttribute("titleWidth", title.map(s -> 10 + 6 * s.length()).orElse(0))
-				.addAttribute("titleColor", color)
+				.addAttribute("titleColor", titleColor.charAt(0) == '#' ? titleColor : '#' + titleColor)
 				.addAttribute("scale", scale)
 				.addAttribute("progress", progress)
 				.addAttribute("progressWidth", width.isPresent() ? width.get() : title.isPresent() ? 60 : 90)
